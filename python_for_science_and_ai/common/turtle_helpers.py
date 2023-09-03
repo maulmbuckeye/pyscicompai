99c4cd6cb9ce8bc22d37598a92133c5ac785aeaa
MIke Ulm <maulmbuckeye@gmail.com>
2023-09-03 16:49:56 -0400

    Made context managers to handle cleanup

--- python_for_science_and_ai/common/turtle_helpers.py
+++ python_for_science_and_ai/common/turtle_helpers.py
@@ -1,13 +1,23 @@
+import random
 import turtle as t
 
 
-def render_quickly():
-    t.delay(0)
-    t.tracer(1000)
-    t.speed('fastest')
+class render_quickly:
+
+    def __enter__(self):
+        t.delay(0)
+        t.tracer(1000)
+        t.speed('fastest')
+
+    def __exit__(self, type, value, traceback):
+        t.update()
 
 
-def setup_start(x,y, heading):
+def setup_start(x, y, heading):
     t.penup()
-    t.setpos(x,y)
+    t.setpos(x, y)
     t.setheading(heading)
+
+
+def jitter(amount):
+    return random.normalvariate(0, amount)
--- python_for_science_and_ai/fractal_tree/fractal_tree.py
+++ python_for_science_and_ai/fractal_tree/fractal_tree.py
@@ -1,17 +1,17 @@
-import random
+
 from turtle import *
 import common.turtle_helpers as th
 
 
 def main():
-    th.render_quickly()
-    th.setup_start(0, -250, 90)
 
     levels = numinput("Parameters", "Depth of branches off trunk",
                       default=9, minval=0, maxval=15)
 
-    draw_branch(200, levels)
-    update()  # Make sure something is shown. Number of drawing actings may be less than tracer value
+    with th.render_quickly():
+        th.setup_start(0, -250, 90)
+        draw_branch(200, levels)
+
     input("Hit enter to end")
 
 
@@ -28,21 +28,32 @@ def draw_branch(length, depth_of_this_branch):
     pensize(length/10)
     pencolor("green" if length < 20 else "brown")
 
-    this_branch_length = length + random.normalvariate(0, length * 0.25)
-    pendown()
-    fd(this_branch_length)
-    draw_sub_branch(turn_right=30,
-                    sub_branch_length=length * 0.7,
-                    depth_of_parent_branch=depth_of_this_branch)
-    draw_sub_branch(turn_right=-60,
-                    sub_branch_length=length * 0.5,
-                    depth_of_parent_branch=depth_of_this_branch)
-    penup()
-    bk(this_branch_length)
+    with draw_this_branch(length + th.jitter(length * 0.25)):
+        draw_sub_branch(turn_right=30,
+                        sub_branch_length=length * 0.7,
+                        depth_of_parent_branch=depth_of_this_branch)
+        draw_sub_branch(turn_right=-60,
+                        sub_branch_length=length * 0.5,
+                        depth_of_parent_branch=depth_of_this_branch)
+
+
+class draw_this_branch:
+    _length = 0
+
+    def __init__(self, this_branch_length):
+        self._length = this_branch_length
+
+    def __enter__(self):
+        pendown()
+        fd(self._length)
+
+    def __exit__(self, type, value, traceback):
+        penup()
+        bk(self._length)
 
 
 def draw_sub_branch(turn_right, sub_branch_length, depth_of_parent_branch):
-    this_turn = turn_right + random.normalvariate(0, 5)
+    this_turn = turn_right + th.jitter(5)
     rt(this_turn)
     draw_branch(sub_branch_length, depth_of_parent_branch - 1)
     lt(this_turn)
