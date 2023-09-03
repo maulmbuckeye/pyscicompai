28fcfd7d8e3ddd14f17231d979891520fbec4588
MIke Ulm <maulmbuckeye@gmail.com>
2023-09-03 17:03:51 -0400

    Made clean exit if cancel in levels dialog

--- python_for_science_and_ai/fractal_tree/fractal_tree.py
+++ python_for_science_and_ai/fractal_tree/fractal_tree.py
@@ -5,14 +5,22 @@ import common.turtle_helpers as th
 
 def main():
 
-    levels = numinput("Parameters", "Depth of branches off trunk",
-                      default=9, minval=0, maxval=15)
+    levels = get_levels()
 
     with th.render_quickly():
         th.setup_start(0, -250, 90)
         draw_branch(200, levels)
 
     input("Hit enter to end")
+
+
+def get_levels():
+    levels = numinput("Parameters", "Depth of branches off trunk",
+                      default=9, minval=0, maxval=15)
+    if levels is None:
+        print("Thanks for playing")
+        exit()
+    return levels
 
 
 def draw_branch(length, depth_of_this_branch):
@@ -29,12 +37,14 @@ def draw_branch(length, depth_of_this_branch):
     pencolor("green" if length < 20 else "brown")
 
     with draw_this_branch(length + th.jitter(length * 0.25)):
-        draw_sub_branch(turn_right=30,
-                        sub_branch_length=length * 0.7,
-                        depth_of_parent_branch=depth_of_this_branch)
-        draw_sub_branch(turn_right=-60,
-                        sub_branch_length=length * 0.5,
-                        depth_of_parent_branch=depth_of_this_branch)
+        std = [(30, length * 0.7), (-60, length * 0.5)] # noqa
+        three_branch = [(35, length * 0.7),  # noqa
+                        (-70, length * 0.5),
+                        (-10, length * 0.5)]
+        for angle, l in three_branch:
+            draw_sub_branch(turn_right=angle,
+                            sub_branch_length=l,
+                            depth_of_parent_branch=depth_of_this_branch)
 
 
 class draw_this_branch:
@@ -47,7 +57,7 @@ class draw_this_branch:
         pendown()
         fd(self._length)
 
-    def __exit__(self, type, value, traceback):
+    def __exit__(self, t, v, tb):
         penup()
         bk(self._length)
 
