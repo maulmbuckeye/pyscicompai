1443d64b4c208ba34d76b2002ff42ea634b6c5b0
MIke Ulm <maulmbuckeye@gmail.com>
2023-09-11 16:00:39 -0400

    Makes curve more complex

--- python_for_science_and_ai/Koch_curve/KochCurve.py
+++ python_for_science_and_ai/Koch_curve/KochCurve.py
@@ -1,6 +1,8 @@
 from turtle import *
+from math import acos, pi
 
 speed(0)
+tracer(1000)
 
 
 def KochCurve(length, level):  # noqa
@@ -8,17 +10,23 @@ def KochCurve(length, level):  # noqa
         fd(length)
         return
 
-    KochCurve(length/3, level-1)
-    lt(60)
-    KochCurve(length/3, level-1)
-    rt(120)
-    KochCurve(length/3, level-1)
-    lt(60)
-    KochCurve(length/3, level-1)
+    flat_section = 0.48
+    to_peak_section = 0.5 - flat_section
+    angle = acos(to_peak_section / flat_section) * 180 / pi
+
+    instuctions = [(0, 1/3), (60, 1/3), (-120, 1/3), (60, 1/3)]
+
+    instuctions = [(0, flat_section), (angle, flat_section), (-2 * angle, flat_section), (angle, flat_section)]
+    for turn, percent_of_length in instuctions:
+        lt(turn)
+        KochCurve(length * percent_of_length, level - 1)
 
 
 if __name__ == '__main__':
-    KochCurve(300, 4)
+    full_length = 500
+
+    KochCurve(400, 3)
+    update()
     input("Hit enter to finish")
 
 
