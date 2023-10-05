63fb74a47b8c9854eea3d9d0203f5965fb3b5f4a
MIke Ulm <maulmbuckeye@gmail.com>
2023-10-05 15:33:37 -0400

    Pulled from PythonExercises and pruned

--- Factoring2/README.md
+++
@@ -1,8 +0,0 @@
-# Prime Factors Kata, Part II
-
-Second attempt.
-
-Compares well with UnBob's kata, http://butunclebob.com/ArticleS.UncleBob.ThePrimeFactorsKata
-
-Uses tail recursion. Close to functional
-
--- Factoring2/fac.py
+++
@@ -1,5 +0,0 @@
-def fac(n: int) -> list[int]:
-    for possible_factor in range(2, n+1):
-        if n % possible_factor == 0:
-            return [possible_factor] + fac(n // possible_factor)
-    return []
